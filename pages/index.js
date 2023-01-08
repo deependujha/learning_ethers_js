d68e4a46726069ecaba3924044c5159edd4faff1
deependujha <deependujha21@gmail.com>
2023-01-08 23:15:13 +0530

    added code for how to get signature & verify it

--- pages/index.js
+++ pages/index.js
@@ -62,21 +62,39 @@ const index = () => {
 				to: '0x31B0F3eeD8cAFA7D09C862b7779AAc826F3c4468',
 				value: ethers.utils.parseEther('0.05'),
 			})
-			.then(async(tx) => {
+			.then(async (tx) => {
 				console.log('tx occurred: ', tx);
-        let receipt = await tx.wait()
-        console.log("receipt is: ", receipt);
-
+				let receipt = await tx.wait();
+				console.log('receipt is: ', receipt);
 			})
 			.catch((err) => {
 				console.log('error occurred: ', err);
 			});
 	};
 
+	const signMessage = async () => {
+		const provider = new ethers.providers.Web3Provider(window.ethereum);
+		await provider.send('eth_requestAccounts', []);
+		const signer = await provider.getSigner();
+		const signature = await signer.signMessage('Hello World');
+		console.log('signature is: ', signature);
+	};
+
+	const verifyMsg = async () => {
+		const sign =
+			'0x8dd0b6e7610f1c43a914e6065b7eeb15c8aee1e5bd20867f40d9b74466026496270e20d4e0b1283c4c6bdbf4e322abf22e910d268ce4441316dcce12d0a228b41c';
+		const msg = 'Hello World';
+		const signerAddr = await ethers.utils.verifyMessage(msg, sign);
+		console.log('signer address is: ', signerAddr);
+		
+	};
+
 	return (
 		<div>
 			<button onClick={btnClick}>Click me</button>
 			<button onClick={sendEth}>Send ether</button>
+			<button onClick={signMessage}>sing a message</button>
+			<button onClick={verifyMsg}>verify message</button>
 		</div>
 	);
 };
